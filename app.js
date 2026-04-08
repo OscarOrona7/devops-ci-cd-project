d14ac7d19f6403df5671472bf96d6109ae752494
Oscar Garcia <OscarGarcia7@my.unt.edu>
2026-04-08 05:19:01 -0500

    Day 1 complete - working Node app

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>CI/CD DevOps Project 🚀</h1><p>Deployed via AWS + GitHub Actions</p>');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
